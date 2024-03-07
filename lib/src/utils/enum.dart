ad159a818413f34ef2266aaa8f87af417837694c
Oguzhan Yildirim <108305486+OguzhanYildirimm@users.noreply.github.com>
2024-03-07 16:16:56 +0300

    Update enum.dart

--- lib/src/utils/enum.dart
+++ lib/src/utils/enum.dart
@@ -1,13 +1,1 @@
-enum PredictionStatus {
-  starting,
-  processing,
-  succeeded,
-  failed,
-  canceled;
 
-  static PredictionStatus fromResponseField(String status) {
-    return PredictionStatus.values.firstWhere(
-      (statusEnum) => statusEnum.name.toLowerCase() == status.toLowerCase(),
-    );
-  }
-}
